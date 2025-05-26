a781437640714e776913fd51f07f6d52e00efc14
michal-lev <michallev231@gmail.com>
2025-05-26 17:14:38 +0300

    changed abstract class name

--- src/michal_liora/Doctor.java
+++ src/michal_liora/Doctor.java
@@ -1,6 +1,6 @@
 package michal_liora;
 
-public class Doctor extends LecturerWithArticles {
+public class Doctor extends HighRankLecturer {
     public Doctor(String name, String id, String degreeLevel, String degreeTitle, double salary, Department department) {
         super(name, id, degreeLevel, degreeTitle, salary, department);
     }
--- src/michal_liora/HighRankLecturer.java
+++ src/michal_liora/HighRankLecturer.java
@@ -1,10 +1,10 @@
 package michal_liora;
 
-abstract class LecturerWithArticles extends Lecturer implements Comparable<LecturerWithArticles> {
+abstract class HighRankLecturer extends Lecturer implements Comparable<HighRankLecturer> {
     protected String[] articles;
     protected int articleCount;
 
-    public LecturerWithArticles(String name, String id, String degreeLevel, String degreeTitle, double salary, Department department) {
+    public HighRankLecturer(String name, String id, String degreeLevel, String degreeTitle, double salary, Department department) {
         super(name, id, degreeLevel, degreeTitle, salary, department);
         setArticles(new String[1]);
         setArticleCount(0);
@@ -27,11 +27,10 @@ abstract class LecturerWithArticles extends Lecturer implements Comparable<Lectu
     }
 
     @Override
-    public int compareTo(LecturerWithArticles other) {
+    public int compareTo(HighRankLecturer other) {
         return Integer.compare(getArticleCount(),other.getArticleCount());
     }
     public String articlesToString(){
-
         String articlesStr = "[";
         int i = 0;
         for (; i < (committeesCount - 1); i++){
